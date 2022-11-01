96f752dfbbbffdfd8a3f2686af9b5b86145b10a9
BriceNogue <93254397+BriceNogue@users.noreply.github.com>
2022-11-01 04:13:57 +0100

    .

--- TutosRealise/PokemonApp/src/app/pokemon/list-pokemon/list-pokemon.component.ts
+++ TutosRealise/PokemonApp/src/app/pokemon/list-pokemon/list-pokemon.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { POKEMONS } from '../mock-pokemon-list';
 import { Pokemon } from '../pokemon';
+import { PokemonService } from '../pokemon.service';
 
 @Component({
   selector: 'app-list-pokemon',
@@ -10,10 +10,11 @@ import { Pokemon } from '../pokemon';
 })
 export class ListPokemonComponent implements OnInit {
 
-  pokemonList : Pokemon[]= POKEMONS;
-  constructor(private router: Router) { }
+  pokemonList : Pokemon[];
+  constructor(private router: Router, private pokemonService: PokemonService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.pokemonList = this.pokemonService.getPokemonList();
   }
 
   goToPokemon(pokemon: Pokemon) {
---
+++ TutosRealise/PokemonApp/src/app/pokemon/pokemon.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
+
+@Injectable()
+export class PokemonService {
+
+  constructor() { }
+
+  getPokemonList(): Pokemon[] {
+    return POKEMONS;
+  }
+
+  getPokemonById(pokemonId: number): Pokemon|undefined {
+    return POKEMONS.find(pokemon => pokemon.id == pokemonId);
+  }
+
+  getPokemonTypeList() : String[] {
+    return [
+      'Plante',
+      'Feu',
+      'Eau',
+      'Insecte',
+      'Normal',
+      'Electrik',
+      'Poison',
+      'Fee',
+      'Vol',
+      'Combat',
+      'Psy'
+    ];
+  }
+}

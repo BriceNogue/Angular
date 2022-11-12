eb6aec4ee01d7f83a11eeb36c739234bdc0df2ac
BriceNogue <93254397+BriceNogue@users.noreply.github.com>
2022-11-12 05:07:45 +0100

    Ajout d'un nouveau pokemon

--- TutosRealise/PokemonApp/src/app/pokemon/pokemon-form/pokemon-form.component.ts
+++ TutosRealise/PokemonApp/src/app/pokemon/pokemon-form/pokemon-form.component.ts
@@ -12,11 +12,13 @@ export class PokemonFormComponent implements OnInit {
 
   @Input() pokemon : Pokemon;
   types : string[];
+  isAddForm: boolean;
 
   constructor(private route: ActivatedRoute, private router: Router, private pokemonService: PokemonService) { }
 
   ngOnInit(): void {
     this.types = this.pokemonService.getPokemonTypeList();
+    this.isAddForm = this.router.url.includes('add');
   }
 
   hasType(type: string): boolean {
@@ -48,8 +50,13 @@ export class PokemonFormComponent implements OnInit {
   }
 
   onSubmit() {
-    this.pokemonService.updatePokemon(this.pokemon)
-      .subscribe(() => this.router.navigate(['/pokemon', this.pokemon.id]));
+    if(this.isAddForm) {
+       this.pokemonService.addPokemon(this.pokemon)
+        .subscribe((pokemon: Pokemon) => this.router.navigate(['/pokemon', pokemon.id]));
+    } else {
+      this.pokemonService.updatePokemon(this.pokemon)
+        .subscribe(() => this.router.navigate(['/pokemon', this.pokemon.id]));
+    }
   }
 
 }

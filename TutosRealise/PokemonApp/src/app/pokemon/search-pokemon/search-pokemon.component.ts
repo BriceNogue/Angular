010dd562033efc0b8b9481658080d53d4b8cb9c1
BriceNogue <93254397+BriceNogue@users.noreply.github.com>
2022-11-12 05:51:52 +0100

    Search pokemon

---
+++ TutosRealise/PokemonApp/src/app/pokemon/search-pokemon/search-pokemon.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, Subject } from 'rxjs';
+import { Pokemon } from '../pokemon';
+
+@Component({
+  selector: 'app-search-pokemon',
+  templateUrl: './search-pokemon.component.html',
+  styleUrls: ['./search-pokemon.component.css']
+})
+export class SearchPokemonComponent implements OnInit {
+
+  searchTerms = new Subject<string>();
+  pokemons$: Observable<Pokemon[]>;
+  
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  search(term: string) {
+
+  }
+
+  goToDetail(pokemon: Pokemon) {
+    const link = ['/pokemon', pokemon.id];
+    this.router.navigate(link);
+  }
+
+}
